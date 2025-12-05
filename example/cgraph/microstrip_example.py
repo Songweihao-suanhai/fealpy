d5b5feb42d39b3c27da3e9a22f319d3c48b8d7b9
Mualani <zhengyy0621@foxmail.com>
2025-12-05 11:14:22 +0800

    feat(cgraph): update node names and titles for microstrip antenna. Update example file.

--- example/cgraph/microstrip_example.py
+++ example/cgraph/microstrip_example.py
@@ -4,29 +4,45 @@ import fealpy.cgraph as cgraph
 
 logger.setLevel("DEBUG")
 mesher = cgraph.create("MicrostripPatchMesher3d")
-msa = cgraph.create("MicrostripAntenna3D")
+msa = cgraph.create("TimeHarmonicMaxwellWithLumpedPort")
 solver = cgraph.create("IterativeSolver")
+postprocess = cgraph.create("AntennaPostprocess")
 
 msa(
     mesh=mesher().mesh,
+    p=0,
+    f=1.575,
     sub_region=mesher().sub,
     air_region=mesher().air,
     pec_face=mesher().pec,
     lumped_edge=mesher().lumped,
-    r0=80.0,
-    r1=100.0
+    r0=100.0,
+    r1=120.0
 )
 solver(
     A=msa().operator,
     b=msa().vector,
     solver="minres"
 )
-
+postprocess(
+    uh=solver(),
+    mesh=mesher().mesh,
+    p=0
+)
 
 g1 = cgraph.Graph()
-g1.output(uh=solver())
+g1.output(
+    e=postprocess().E,
+    # sub=mesher().sub,
+    # space=msa().space,
+    mesh=mesher().mesh,
+    # A=msa().operator
+)
 g1.register_error_hook(lambda x: print(x.traceback))
 g1.execute()
 result = g1.get()
 
-print(result)
+mesh = result["mesh"]
+E = result["e"]
+mesh.celldata['E'] = E
+mesh.to_vtk("msmesh.vtu")
