ab3b6fd767a86a2195737f24dda7460ef0f3c1ed
BellaLq <2655493031@qq.com>
2025-12-18 14:21:17 +0800

    wip(cgraph):优化槽型梁和列车车轴模型节点.

--- fealpy/cgraph/material/beam_material.py
+++ fealpy/cgraph/material/beam_material.py
@@ -2,8 +2,7 @@ from typing import Union
 from ..nodetype import CNodeType, PortConf, DataType
 
 __all__ = ["BeamMaterial", 
-           "ChannelBeamMaterial",
-           "TimoMaterial"]
+           "ChannelBeamMaterial"]
 
 
 class BeamMaterial(CNodeType):
@@ -114,61 +113,5 @@ class ChannelBeamMaterial(CNodeType):
         return tuple(
             getattr(beam_material, name)
             for name in ["E", "nu", "mu", "rho"]
-        )
-        
-
-class TimoMaterial(CNodeType):
-    r"""Timoshenko Beam Material Definition Node.
-    
-        Inputs:
-            property (STRING): Material type, e.g., "Steel".
-            beam_type (MENU): Beam model type selection.
-            beam_para (TENSOR): Beam section parameters, each row represents [Diameter, Length, Count].
-            axle_para (TENSOR): Axle section parameters, each row represents [Diameter, Length, Count].
-            E (FLOAT): Elastic modulus of the beam material.
-            nu (FLOAT): Poisson's ratio of the beam material.
-
-        Outputs:
-            E (FLOAT): Elastic modulus of the beam material.
-            nu (FLOAT): Poisson's ratio of the beam material.
-            mu (FLOAT): Shear modulus, computed as `E / [2(1 + nu)]`.
-    """
-    TITLE: str = "列车车轴梁段部分材料属性"
-    PATH: str = "material.solid"
-    INPUT_SLOTS = [
-        PortConf("property", DataType.MENU, 0, desc="材料名称", title="材料材质", default="structural-steel", 
-                 items=["structural-steel", "aluminum", "concrete", "plastic", "wood", "alloy"]),
-        PortConf("type", DataType.MENU, 0, desc="车轴材料类型选择", title="梁类型", default="Timoshenko", 
-                 items=["Euler-Bernoulli", "Timoshenko"]),
-        PortConf("beam_para", DataType.TENSOR, 1, desc="梁结构参数数组，每行为 [直径, 长度, 数量]", title="梁段参数"),
-        PortConf("axle_para", DataType.TENSOR, 1, desc="轴结构参数数组，每行为 [直径, 长度, 数量]", title="轴段参数"),
-        PortConf("E", DataType.FLOAT, 0, desc="梁的弹性模量", title="梁的弹性模量", default=2.1e11),
-        PortConf("nu", DataType.FLOAT, 0, desc="梁的泊松比", title="梁的泊松比", default=0.3)
-    ]
-    
-    OUTPUT_SLOTS = [
-        PortConf("E", DataType.FLOAT, title="梁的弹性模量"),
-        PortConf("nu", DataType.FLOAT, title="梁的泊松比"),
-        PortConf("mu", DataType.FLOAT, title="梁的剪切模量")
-    ]
-    
-    @staticmethod
-    def run(**options):
-        from fealpy.csm.model.beam.timobeam_axle_data_3d import TimobeamAxleData3D
-        from fealpy.csm.material import TimoshenkoBeamMaterial
-
-        model = TimobeamAxleData3D(
-            beam_para=options.get("beam_para"),
-            axle_para=options.get("axle_para")
-        )
-
-        beam_material = TimoshenkoBeamMaterial(model=model, 
-                                        name=options.get("type"),
-                                        elastic_modulus=options.get("E"),
-                                        poisson_ratio=options.get("nu"))
-
-        return tuple(
-            getattr(beam_material, name)
-            for name in ["E", "nu", "mu"]
         )
         
--- fealpy/cgraph/material/linear_elastic_material.py
+++ fealpy/cgraph/material/linear_elastic_material.py
@@ -1,7 +1,6 @@
 from ..nodetype import CNodeType, PortConf, DataType
 
-__all__ = ["LinearElasticMaterial", 
-           "AxleMaterial"]
+__all__ = ["LinearElasticMaterial"]
 
  
 class LinearElasticMaterial(CNodeType):
@@ -101,60 +100,3 @@ class LinearElasticMaterial(CNodeType):
         lambda_ = E * nu / ((1.0 + nu) * (1.0 - 2.0 * nu))
 
         return E, nu, rho, mu, lambda_
-
-
-class AxleMaterial(CNodeType):
-    r"""Axle Material Definition Node.
-    
-    Inputs:
-        property(MENU): Material name, e.g."structural-steel".
-        type(STRING): Type of bar material.
-        beam_para (TENSOR): Beam section parameters, each row represents [Diameter, Length, Count].
-        axle_para (TENSOR): Axle section parameters, each row represents [Diameter, Length, Count].
-        stiffness (FLOAT): spring stiffness.
-        E (FLOAT): Elastic modulus of the axle material.
-        nu (FLOAT): Poisson's ratio of the axle material.
-
-        Outputs:
-            E (FLOAT): Elastic modulus of the axle material.
-            nu (FLOAT): Poisson's ratio of the axle material.
-            mu (FLOAT): Shear modulus of the axle material.
-
-    """
-    TITLE: str = "列车车轴弹簧部分材料属性"
-    PATH: str = "material.solid"
-    INPUT_SLOTS = [
-        PortConf("property", DataType.MENU, 0, desc="材料名称", title="材料材质", default="structural-steel", 
-                 items=["structural-steel", "aluminum", "concrete", "plastic", "wood", "alloy"]),
-        PortConf("type", DataType.STRING, 0, desc="材料类型", title="弹簧类型", default="spring"),
-        PortConf("beam_para", DataType.TENSOR, 1, desc="梁结构参数数组，每行为 [直径, 长度, 数量]", title="梁段参数"),
-        PortConf("axle_para", DataType.TENSOR, 1, desc="轴结构参数数组，每行为 [直径, 长度, 数量]", title="轴段参数"),
-        PortConf("stiffness", DataType.FLOAT, 0, desc="弹簧刚度", title="弹簧的刚度", default=1.976e6),
-        PortConf("E", DataType.FLOAT, 0, desc="弹簧弹性模量", title="弹簧的弹性模量", default=1.976e6),
-        PortConf("nu", DataType.FLOAT, 0, desc="弹簧泊松比", title="弹簧的泊松比", default=-0.5)
-    ]
-    
-    OUTPUT_SLOTS = [
-        PortConf("E", DataType.FLOAT, title="弹簧的弹性模量"),
-         PortConf("nu", DataType.FLOAT, title="弹簧的泊松比"),
-        PortConf("mu", DataType.FLOAT, title="弹簧的剪切模量")
-    ]
-    
-    @staticmethod
-    def run(**options):
-        from fealpy.csm.model.beam.timobeam_axle_data_3d import TimobeamAxleData3D
-        from fealpy.csm.material import AxleMaterial
-
-        model = TimobeamAxleData3D(
-            beam_para=options.get("beam_para"),
-            axle_para=options.get("axle_para")
-        )
-
-        axle_material = AxleMaterial(model=model,
-                                name=options.get("type"),
-                                elastic_modulus=options.get("E"),
-                                poisson_ratio=options.get("nu")
-                            )
-        return tuple(
-            getattr(axle_material, name) for name in ["E", "nu", "mu"]
-        )
