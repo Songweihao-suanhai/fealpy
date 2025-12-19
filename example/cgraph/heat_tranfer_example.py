1b9510721c269fdf27306668e50966d340570d29
zhr-edwin <2533537314@qq.com>
2025-12-19 11:20:45 +0800

    feat(cgraph):use the loop node to handle the scenario of heat transfer on a flat plate, and split the adjacent search into a general node.

--- example/cgraph/heat_tranfer_example.py
+++ example/cgraph/heat_tranfer_example.py
@@ -4,18 +4,83 @@ from fealpy.backend import backend_manager as bm
 bm.set_backend("pytorch")
 WORLD_GRAPH = cgraph.WORLD_GRAPH
 generation = cgraph.create("HeatTransferParticleGeneration")
-iterative = cgraph.create("HeatTransferParticleIterativeUpdate")
-
+SPHQuery1 = cgraph.create("SPHQuery")
+iterative1 = cgraph.create("HeatTransferParticleIterativeUpdate")
+SPHQuery2 = cgraph.create("SPHQuery")
+iterative2 = cgraph.create("HeatTransferParticleIterativeUpdate")
+SPHQuery3 = cgraph.create("SPHQuery")
+iterative3 = cgraph.create("HeatTransferParticleIterativeUpdate")
 generation(
     dx=0.02,
     dy=0.02,
 )
+SPHQuery1(
+    mesh = generation().mesh,
+    kernel = "quintic",
+    dx=0.02,
+    space = True,
+    box_size = generation().box_size,
+)
+iterative1(mesh = generation().mesh,
+        i=0,
+        box_size = generation().box_size,
+        dx=0.02, 
+        dt=0.00045454545454545455,
+        neighbors=SPHQuery1().neighbors,
+        self_node=SPHQuery1().self_node,
+        dr=SPHQuery1().dr,
+        dist=SPHQuery1().dist,
+        w=SPHQuery1().w,
+        grad_w=SPHQuery1().grad_w,
+        grad_w_norm=SPHQuery1().grad_w_norm,
+        output_dir="/home/edwin/output")
 
-iterative(mesh = generation().mesh, maxstep=1000, dx=0.02, kernel="quintic", 
-        dt=0.00045454545454545455,output_dir="/home/edwin/output")
-WORLD_GRAPH.output(velocity=iterative().velocity, 
-                   pressure=iterative().pressure,
-                   temperature=iterative().temperature)
+SPHQuery2(
+    mesh = iterative1().mesh,
+    kernel = "quintic",
+    dx=0.02,
+    space = True,
+    box_size = generation().box_size,
+)
+iterative2(mesh = iterative1().mesh,
+        i=1,
+        box_size = generation().box_size,
+        dx=0.02, 
+        dt=0.00045454545454545455,
+        neighbors=SPHQuery2().neighbors,
+        self_node=SPHQuery2().self_node,
+        dr=SPHQuery2().dr,
+        dist=SPHQuery2().dist,
+        w=SPHQuery2().w,
+        grad_w=SPHQuery2().grad_w,
+        grad_w_norm=SPHQuery2().grad_w_norm,
+        output_dir="/home/edwin/output")
+
+SPHQuery3(
+    mesh = iterative2().mesh,
+    kernel = "quintic",
+    dx=0.02,
+    space = True,
+    box_size = generation().box_size,
+)
+iterative3(mesh = iterative2().mesh,
+        i=2,
+        box_size = generation().box_size,
+        dx=0.02, 
+        dt=0.00045454545454545455,
+        neighbors=SPHQuery3().neighbors,
+        self_node=SPHQuery3().self_node,
+        dr=SPHQuery3().dr,
+        dist=SPHQuery3().dist,
+        w=SPHQuery3().w,
+        grad_w=SPHQuery3().grad_w,
+        grad_w_norm=SPHQuery3().grad_w_norm,
+        output_dir="/home/edwin/output")
+
+
+WORLD_GRAPH.output(velocity=iterative3().velocity, 
+                   pressure=iterative3().pressure,
+                   temperature=iterative3().temperature)
 
 # 最终连接到图输出节点上
 WORLD_GRAPH.register_error_hook(print)
